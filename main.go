788775b9c2c7fb063c1457e39e8e4b6373d80cb6
Cristiano Altmann <cristiano.altmann@mercadolivre.com>
2021-06-29 16:07:23 -0300

    Adicionando validação de erro

--- main.go
+++ main.go
@@ -20,7 +20,19 @@ func main() {
 	s, err := exampleOne()
 	if err != nil {
 		fmt.Println("err:", err)
-		return
+
+		//validando por tipo
+		ce, ok := err.(*custom_error.CustomError)
+		if ok {
+			switch ce.Type.Codigo {
+			case custom_error.Bussines_Error_1:
+				fmt.Println("Error 1")
+			case custom_error.Bussines_Error_2:
+				fmt.Println("Error 2")
+			default:
+				fmt.Println("Não encontrado.")
+			}
+		}
 	}
 	fmt.Println(s)
 }
